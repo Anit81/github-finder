e36430a31f9cc859a96a800f200efa38ad313ab1
IBESI FAUSTINA <faustinaibesi88@gmail.com>
2025-10-12 03:11:08 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -33,8 +33,11 @@ function App() {
       {error && <p style={{ color: "red" }}>{error}</p>}
       { finder &&
         <>
-        <p className='name'>User Name:{finder.name}</p>
+          <div className="combine">
+          <p className='name'>User Name:{finder.name}</p>
         <img src={finder.avatar_url} alt={finder.login}width={100} height={100}  /> 
+          </div>
+        
         <p className='bio'>Bio: {finder.bio}</p>
         <p className='repo'>Repository: {finder.public_repos}</p>
       </>
